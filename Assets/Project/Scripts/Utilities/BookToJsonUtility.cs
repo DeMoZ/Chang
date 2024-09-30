12a0e25d478b4590a88121271bbab691288c6cd3
Roman Kolychev <demozbox@gmail.com>
2024-10-01 01:07:41 +0700

    Save game book as json file in bundles

---
+++ Assets/Project/Scripts/Utilities/BookToJsonUtility.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Sirenix.OdinInspector;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BookToJsonUtility", menuName = "Chang/GameBook/BookToJsonUtility", order = 0)]
+public class BookToJsonUtility : ScriptableObject
+{
+    public GameBookConfig GameBookConfig;
+    public TextAsset GameBookJson;
+
+    [Button]
+    void MakeJson()
+    {
+        Debug.Log($"{nameof(MakeJson)} Start");
+        var result = new List<LessonNames>();
+
+        foreach (var lesson in GameBookConfig.Lessons)
+        {
+            var assetPath = AssetDatabase.GetAssetPath(lesson);
+            var fileName = Path.GetFileNameWithoutExtension(assetPath);
+
+            result.Add(new LessonNames()
+            {
+                FileName = fileName,
+                AssetPath = assetPath,
+            });
+        }
+
+        var json = JsonConvert.SerializeObject(result, Formatting.Indented);
+        File.WriteAllText(AssetDatabase.GetAssetPath(GameBookJson), json);
+
+        AssetDatabase.Refresh();
+        Debug.Log($"{nameof(MakeJson)} Done");
+    }
+}
+
+[Serializable]
+public struct LessonNames
+{
+    public string FileName;
+    public string AssetPath;
+}
