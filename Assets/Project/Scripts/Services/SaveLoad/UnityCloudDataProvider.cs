5cb2033734b56303af49fd16bd20ec1bd7086dcb
Roman Kolychev <demozbox@gmail.com>
2024-12-22 01:30:31 +0700

    fixed loading progress in web build

--- Assets/Project/Scripts/Services/SaveLoad/UnityCloudDataProvider.cs
+++ Assets/Project/Scripts/Services/SaveLoad/UnityCloudDataProvider.cs
@@ -12,7 +12,7 @@ namespace Chang.Services.DataProvider
 {
     public class UnityCloudDataProvider : IDataProvider
     {
-        private JsonSerializerSettings _jSettings = new()
+        private readonly JsonSerializerSettings _jSettings = new()
         {
             Formatting = Formatting.Indented,
         };
@@ -66,40 +66,58 @@ namespace Chang.Services.DataProvider
                 return;
 
             var dataDict = new Dictionary<string, object> { { key, data } };
-            await CloudSaveService.Instance.Data.Player.SaveAsync(dataDict);
-            Debug.Log($"{key} saved.");
 
+            try
+            {
+                await CloudSaveService.Instance.Data.Player.SaveAsync(dataDict);
+                Debug.Log($"{key} saved.");
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Error on saving data type: {typeof(T).Name}, for key: {key}, error:\n{e}");
+                HandleError(e);
+            }
+        }
+       
+        private async UniTask<T> LoadDataAsync<T>(string key) where T : class
+        {
+            try
+            {
+                var savedData = await CloudSaveService.Instance.Data.Player.LoadAsync(new HashSet<string> { key });
+                var rawJson = JsonConvert.SerializeObject(savedData);
+                Debug.Log($"Loaded raw data for key: {key}:\n{rawJson}");
+            
+                if (savedData.TryGetValue(key, out var value))
+                {
+                    var jsonString = JsonConvert.SerializeObject(value.Value, _jSettings);
+                    Debug.Log($"Extract type: {typeof(T).Name}, for key: {key}:\n{jsonString}");
+
+                    var deserializedObject = JsonConvert.DeserializeObject<T>(jsonString);
+                    return deserializedObject;
+                }
+                
+                Debug.LogWarning($"No saved data found for key: {key}");
+                return default;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Error on loading data type: {typeof(T).Name}, for key: {key}, error:\n{e}");
+                HandleError(e);
+                return default;
+            }
+        }
+        
+        private void HandleError(Exception e)
+        {
             // todo roman solve exceptions
             // <exception cref="CloudSaveException">Thrown if request is unsuccessful.</exception>
             // <exception cref="CloudSaveValidationException">Thrown if the service returned validation error.</exception>
             // <exception cref="CloudSaveRateLimitedException">Thrown if the service returned rate limited error.</exception>
+            // todo roman add error handling, probably internet issue
+
+            throw new NotImplementedException(e.ToString());
         }
-
-        private async UniTask<T> LoadDataAsync<T>(string key) where T : class
-        {
-            var savedData = await CloudSaveService.Instance.Data.Player.LoadAsync(new HashSet<string> { key });
-            
-            var wholeJson = JsonConvert.SerializeObject(savedData, _jSettings);
-            Debug.Log($"Loaded data for key {key}:\n {wholeJson}");
-            
-            if (savedData.TryGetValue(key, out var value))
-            {
-                var valString = JsonConvert.SerializeObject(value, _jSettings);
-                Debug.Log($"Loaded data for key {key}:\n {valString}");
-
-                var jsonObject = JsonConvert.DeserializeObject<Dictionary<string, object>>(valString);
-                if (jsonObject != null && jsonObject.TryGetValue("Value", out var valuePart))
-                {
-                    var valueString = JsonConvert.SerializeObject(valuePart, _jSettings);
-                    var valObject = JsonConvert.DeserializeObject<T>(valueString);
-                    return valObject;
-                }
-            }
-
-            Debug.LogWarning($"No saved data found for key: {key}");
-            return default;
-        }
-
+        
         public void Dispose()
         {
         }
