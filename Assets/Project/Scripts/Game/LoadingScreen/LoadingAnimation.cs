251d62597b97f4acedf90d58ba4757e532b3ad18
Roman Kolychev <demozbox@gmail.com>
2025-06-04 02:36:07 +0700

    Loading animation changed.

--- Assets/Project/Scripts/Game/LoadingScreen/LoadingAnimation.cs
+++ Assets/Project/Scripts/Game/LoadingScreen/LoadingAnimation.cs
@@ -1,21 +1,50 @@
 using System.Collections;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class LoadingAnimation : MonoBehaviour
+namespace Chang
 {
-    [SerializeField] private float speed = 100;
-    private void OnEnable()
+    public class LoadingAnimation : MonoBehaviour
     {
-        StartCoroutine(Rotate());
-    }
-        
-    private IEnumerator Rotate()
-    {
-        transform.localRotation = Quaternion.identity;
-        while (true)
+        [SerializeField] private Image up;
+        [SerializeField] private Image down;
+        [SerializeField] private float speed = 100;
+
+        private void OnEnable()
         {
-            transform.Rotate(0, 0, 1 * Time.deltaTime * speed);
-            yield return null;
+            StartCoroutine(Rotate());
+        }
+
+        private IEnumerator Rotate()
+        {
+            transform.localRotation = Quaternion.identity;
+            var duration = 0.01f * speed;
+            
+            while (true)
+            {
+                up.gameObject.SetActive(true);
+                down.gameObject.SetActive(false);
+                up.fillAmount = 0f;
+                
+                var upTween = DOTween.To(
+                    () => up.fillAmount,
+                    x => up.fillAmount = x,
+                    1f, duration).SetEase(Ease.Linear);
+                
+                yield return upTween.WaitForCompletion();
+                
+                up.gameObject.SetActive(false);
+                down.gameObject.SetActive(true);
+                down.fillAmount = 1f;
+
+                var downTween = DOTween.To(
+                    () => down.fillAmount,
+                    x => down.fillAmount = x,
+                    0f, duration).SetEase(Ease.Linear);
+                
+                yield return downTween.WaitForCompletion();
+            }
         }
     }
 }
