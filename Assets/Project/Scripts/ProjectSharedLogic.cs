f58e813ca57a168c345de11d0eb18a101ec599ac
Roman Kolychev <demozbox@gmail.com>
2026-01-26 03:08:59 +0700

    log and localization keys calculation changed.

--- Assets/Project/Scripts/ProjectSharedLogic.cs
+++ Assets/Project/Scripts/ProjectSharedLogic.cs
@@ -9,6 +9,6 @@ public static class ProjectSharedLogic
     public static string VOCABULARY_QUESTION_LOCALIZATION_KEY(string learnLanguage, string sectionName, string word) => $"{learnLanguage}/Vocabulary/{sectionName}/{word}";
     
     public static string SENTENCE_LESSON_KEY(string learnLanguage, string sectionName, int index) => $"{learnLanguage}.{sectionName}.{index}";
-    public static string SENTENCE_QUESTION_KEY(string learnLanguage, string sectionName, List<string> words) => $"{learnLanguage}/{sectionName}/{string.Join("_", words)}";
-    public static string SENTENCE_QUESTION_LOCALIZATION_KEY(string learnLanguage, string sectionName, List<string> words) => $"{learnLanguage}/Sentences/{sectionName}/{string.Join("_", words)}";
+    public static string SENTENCE_QUESTION_KEY(string learnLanguage, string sectionName, List<string> words) => $"{learnLanguage}/{sectionName}/{string.Join("|", words)}";
+    public static string SENTENCE_QUESTION_LOCALIZATION_KEY(string learnLanguage, string sectionName, List<string> words) => $"{learnLanguage}/Sentences/{sectionName}/{string.Join("|", words)}";
 }
